4a927b31ac59b3210ecc10a899652e661e4173d8
OmarNavarro <omar.n.o.92@gmail.com>
2023-08-31 10:19:23 +0200

    auth

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -3,7 +3,7 @@ const { expressjwt: jwt } = require("express-jwt");
 const isAuthenticated = jwt({
   secret: process.env.TOKEN_SECRET,
   algorithms: ["HS256"],
-  requestProperty: "payload", // despues de validar el token, nos devuelve el valor del payload
+  requestProperty: "payload", 
   getToken: (req) => {
     console.log(req.headers)
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,12 +5,13 @@ const User = require("../models/User.model");
 const jwt = require("jsonwebtoken")
 const isAuthenticated = require("../middlewares/isAuthenticated")
 
-// POST "/api/auth/signup" => registrar al usuario
+// POST "/api/auth/signup" => Register
 router.post("/signup", async (req, res, next) => {
 
   const { username, email, password } = req.body
   console.log(req.body)
 
+  //************
   // 1. validaciones.
   // - los campos llenos
   // - que nombre tenga un formato especifico
@@ -18,28 +19,26 @@ router.post("/signup", async (req, res, next) => {
   // - que el usuario no esté repetido
 
   if (!username || !email || !password) {
-    res.status(400).json({ errorMessage: "Todos los campos deben estar llenos" })
+    res.status(400).json({ errorMessage: "All fields must be filled" })
     return;
   }
 
   try {
-    // 2. encriptar la contraseña
+    //Encrypting password
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt)
     console.log(hashPassword)
   
-    // 3. crear el usuario en la DB
+    // 3. Creates the user in the DB
     await User.create({
       username,
       email,
       password: hashPassword,
-      // isActive: false,
-      // verifyPassword: "1234"
+
     })
   
-    // "https://mipagina.com/verify-user/idUser/1234"
 
-    res.json("usuario creado")
+    res.json("user created")
     
   } catch (error) {
     next(error)
@@ -54,33 +53,33 @@ router.post("/login", async (req, res, next) => {
   const { email, password } = req.body
   console.log(req.body)
 
-  // 1. Todas las validaciones
+  // 1. Validations
 
   try {
-    //  - Que el usuario exista
+    //  - User exists?
     const foundUser = await User.findOne({ email })
     console.log(foundUser)
     if (foundUser === null) {
-      res.status(400).json({ errorMessage: "Usuario no registrado" })
+      res.status(400).json({ errorMessage: "User is not registered" })
       return;
     }
 
-    //  - Que la contraseña sea correcta
+    //  - Password correct?
     const isPasswordValid = await bcrypt.compare(password, foundUser.password)
     if (isPasswordValid === false) {
-      res.status(400).json({ errorMessage: "Contraseña no valida" })
+      res.status(400).json({ errorMessage: "Password invalid" })
       return;
     }
     
   
-    // ... crear la sesión.
-    // Pero ahora haremos sistema de Tokens
+    // .Session created
+ 
 
-    // En el payload agregamos información que no deberia cambiar del usuario
+    // Info that must not be changed
     const payload = {
       _id: foundUser._id,
       email: foundUser.email,
-      // si tuvieramos roles, tienen que ir acá
+      // si tuvieramos roles, tienen que ir acá*********
     }
   
     const authToken = jwt.sign( 
@@ -98,11 +97,10 @@ router.post("/login", async (req, res, next) => {
 })
 
 
-// GET "/api/auth/verify" => indicar al FE que el usuario está activo en llamadas futuras
+// GET "/api/auth/verify" => Tells FE if user is active
 router.get("/verify", isAuthenticated, (req, res, next) => {
 
-  // ! de ahora en adelante, cada vez que usemos el middleware isAuthenticated ...
-  // ! ... vamos a tener acceso a algo llamado req.payload
+
   console.log(req.payload)
 
   res.json(req.payload)
