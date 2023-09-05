bc37e07106002384e5a454a765123ef6d80d0115
Mark <marcboluda@hotmail.com>
2023-09-05 12:36:21 +0200

    event started

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-  title: String,
-  description: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
   location: {
     type: String,
+    required: true,
     enum: [
         'Álava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Ávila', 'Badajoz', 'Barcelona',
         'Burgos', 'Cáceres', 'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 'Córdoba', 'Cuenca',
@@ -18,7 +25,10 @@ const eventSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
-  img: String
+  img: {
+    type: String,
+    required: true,
+  }
 });
 
 const Event = mongoose.model('Event', eventSchema);
